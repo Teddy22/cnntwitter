07b07fa3c07645518ddc54a180b87346d2e888cb
Teddy <edwachable@gmail.com>
2016-03-09 03:32:04 -0500

    successful feed basic style

--- app.js
+++ app.js
@@ -8,19 +8,57 @@ app.use(express.static('public'));
 app.set('view engine', 'jade');
 
 
+var Twitter = require('twitter-js-client').Twitter;
+var twitterConfig = {
+    	"consumerKey": "mviA3nLoMwAXKZy93JLsgF1uB",
+    	"consumerSecret": "33zwztbltnmRvOmZgxaGJGlX36lXvMah3HMA0rkLp0dYla4gnb",
+    	"accessToken": "97811766-XCI7ILJoAhMelxO3lJofCYcdrriUDI6voN4r48lOh",
+    	"accessTokenSecret": "Uc3KDBtIY137l0NrFF8iWGnp1caLBq4kq5AZnbCkWyeAc",
+    	// "callBackUrl": ""
+}
+
+var twitter = new Twitter(twitterConfig);
+
+
+// landing page
 app.get("/", function(req, res) {
 	res.send("<h1>CNN Twitter Feed</h1>");	
 });
 
 
+// handle post form entered twitter handle at input
 app.post("/twitter/feed/account/:keyword", function(req, res) {
 	console.log("post request received");
 	var keyword = req.params.keyword;
-	var renderVals = {keyword:keyword};
-	res.render('feed', renderVals);
+
+	accountExists(keyword, function(exists) {
+		if(exists) {
+			console.log('account exists');
+			var renderVals = {keyword:keyword};
+			res.render('feed', renderVals);
+		} else {
+			res.send("<h3>Oops! Twitter account does not exist or is not public</h3>");
+		}
+	});
 });
 
 
+// check if twitter handle provided exists
+var accountExists = function(keyword, callback) {
+
+	var errorCallback = function(params, error, success) {
+		callback(false);
+	}
+
+	var successCallback = function(body) {
+		callback(true);
+	}
+
+	twitter.getUser({screen_name: keyword}, errorCallback, successCallback);
+}
+
+
+// open port 8085
 app.listen(8085, function() {
 	console.log("server listening at port 8085...");
 });
--- public/js/feed.js
+++ public/js/feed.js
@@ -1,16 +1,28 @@
-alert('feed');
-
-function getFeed() {
-	var keyword = $("input#keywordInput").val();
-	var feed = $("div#feed");http://www.manutd.com/
-	feed.text(keyword);
+function getFeed(obj) {
+	console.log('get feed button click event');
+	
+	if(obj) {
+		var keyword = $(obj).text();
+	}
+	else {
+		var keyword = $("input#keywordInput").val();
+	}
+		
+	var feed = $("div#feed");
 
 	$.post("/twitter/feed/account/" + keyword, function(data, status) {
 		if(status=='success')
-			//
+			feed.html(data);
 		else
-			alert('error getting feed');
+			alert('error getting twitter feed');
 	});
-	// alert(keyword);
-
 }
+
+var assignHandlers = function() {
+	var items = $('nav');
+
+	console.log(items.children());
+	
+}
+
+assignHandlers();
